1e6006dfd271048434fd017a64e834eefe645afa
ivandjoh <idsta.ivandi@xl.co.id>
2024-06-03 04:38:54 +0800

    added hooks on NavItems

--- src/components/NavItems.tsx
+++ src/components/NavItems.tsx
@@ -1,15 +1,32 @@
 "use client"
 
-import React, {useState} from 'react'
+import React, {useEffect, useRef, useState} from 'react'
 import {PRODUCT_CATEGORIES} from "@/config";
 import NavItem from "@/components/NavItem";
+import {useOnClickOutside} from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
 
     const [activeIndex, setActiveIndex] = useState<null | number>(null);
     const isAnyOpen = activeIndex !== null;
+    const navRef = useRef<HTMLDivElement | null>(null);
+
+    useOnClickOutside(navRef, () => setActiveIndex(null));
+
+    useEffect(() => {
+        const handler = (e: KeyboardEvent) => {
+            if (e.key === 'Escape') {
+                setActiveIndex(null);
+            }
+        }
+
+        document.addEventListener('keydown', handler)
+
+        return () => document.removeEventListener('keydown', handler)
+    },[])
+
     return (
-        <div className='flex gap-4 h-full'>
+        <div className='flex gap-4 h-full' ref={navRef}>
             {PRODUCT_CATEGORIES.map((category, i) => {
 
                 const handleOpen = () => {
